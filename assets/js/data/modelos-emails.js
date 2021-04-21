ed78ba4d0b81912c7239ad8239f4932fc97003a0
brunocriacoes <br.rafael@outlook.com>
2021-04-21 12:13:07 -0300

    add data js

---
+++ assets/js/data/modelos-emails.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 'agradecimento', text: 'Agradecimento', obs: '', link: '/mode-email/agradecimento', feature_flag: true },
+    { id: 'confirmacao_de_recebimento', text: 'Confirmação de Recebimento', obs: '', link: '/mode-email/confirmacao_de_recebimento', feature_flag: true },
+    { id: 'carrinho_abandonado', text: 'Carrinho Abandonado', obs: '', link: '/mode-email/carrinho_abandonado', feature_flag: true },
+    { id: 'tronece_um_assinante', text: 'Tronece um Assinante', obs: '', link: '/mode-email/tronece_um_assinante', feature_flag: true },
+]
---
+++ assets/js/data/modulos.js
@@ -0,0 +1,8 @@
+export default [
+    { id: 'correios', text: 'Correios', logo: '', link: '/correios', feature_flag: true },
+    { id: 'via_cep', text: 'Via CEP', logo: '', link: '/via-cep', feature_flag: true },
+    { id: 'mailing_boss', text: 'Mailing Boss', logo: '', link: '/mailing-boss', feature_flag: true },
+    { id: 'email', text: 'E-mail', logo: '', link: '/Email', feature_flag: true },
+    { id: 'rd_station', text: 'RD Station', logo: '', link: '/rd-station', feature_flag: true },
+    { id: 'evendas', text: 'Evendas', logo: '', link: '/evendas', feature_flag: true },
+]
