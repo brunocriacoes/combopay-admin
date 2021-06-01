cd574bfec2d803d2ff0c9089cfab3d91a8791c75
brunocriacoes <br.rafael@outlook.com>
2021-06-01 09:09:11 -0300

    fix

--- assets/js/module/c-metas.js
+++ assets/js/module/c-metas.js
@@ -12,30 +12,39 @@ export default {
             playload: [],
             name_flag: 'METAS_2021',
             loading: false,
+            error: {
+                status: false,
+                text: 'Salvo com sucesso',
+                type: 'success'
+            }
         }
     },
     async mounted() {
         let res = (await this.Super.flag_all()).data
-        let flag = res.find( post => post.flag == this.name_flag && post.instituicao_id == this.cache.institution )
-        if( !flag ) {
-            let new_flag = await this.Super.flag_post( {
+        let flag = res.find(post => post.flag == this.name_flag && post.instituicao_id == this.cache.institution)
+        if (!flag) {
+            let new_flag = await this.Super.flag_post({
                 flag: this.name_flag,
                 instituicao_id: this.cache.institution,
-                base64: btoa( JSON.stringify(this.playload) )
-            } )
-        }else {
-            let data = JSON.parse( atob( flag.base64 ) )
+                base64: btoa(JSON.stringify(this.playload))
+            })
+        } else {
+            let data = JSON.parse(atob(flag.base64))
             this.playload = data
         }
     },
     methods: {
         async save() {
             this.loading = true
-            let res = await this.Super.flag_put( this.cache.institution, {
+            let res = await this.Super.flag_put(this.cache.institution, {
                 flag: this.name_flag,
                 instituicao_id: this.cache.institution,
-                base64: btoa( JSON.stringify(this.playload) )
-            } )
+                base64: btoa(JSON.stringify(this.playload))
+            })
+            console.log( res )
+            this.error.status = true
+            this.error.text = res.message
+            this.error.type = res.status
             this.loading = false
         }
     }
